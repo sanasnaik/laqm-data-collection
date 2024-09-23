4521d40a3f76daa2a5afbcd871b0f1c00b163789
Sana Naik <ssnaik2104@gmail.com>
2024-09-23 16:42:42 -0400

    Initial GUI progress

--- Sana_Naik_Data_Collection_Code.py
+++ Sana_Naik_Data_Collection_Code.py
@@ -13,29 +13,28 @@ import time
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
+from tkinter import *
 
-# Prepare CSV file path
+# Define constants
 current_time = datetime.datetime.now().strftime("%m-%d-%Y %I.%M%p")
 csv_file_path = f"C:\\Users\\laqm\\Documents\\CSV Data Outputs\\{current_time}.csv"
 fieldnames = ['Time', 'Voltage', 'Frequency', 'Channel1(X)', 'Channel2(Y)']
+my_instrument = pyvisa.ResourceManager().open_resource('GPIB0::8::INSTR')
 
-# Collects data into CSV and displays datapoints as a plot
-def data_collect():
+# Collects data into CSV and plots
+def run():
     timevalue = 0
 
-    # Write header to CSV file
     with open(csv_file_path, mode='w', newline='') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         csv_writer.writeheader()
 
     while True:
-        # Query instrument data
         freq = my_instrument.query_ascii_values('FREQ?')[0]
         voltage = my_instrument.query_ascii_values('SLVL?')[0]
         channel1 = my_instrument.query_ascii_values("OUTP? 1")[0]
         channel2 = my_instrument.query_ascii_values("OUTP? 2")[0]
 
-        # Prepare data for CSV
         info = {
             "Time": timevalue,
             "Voltage": voltage,
@@ -44,12 +43,12 @@ def data_collect():
             "Channel2(Y)": float(channel2)
         }
 
-        # Append data to CSV file
         with open(csv_file_path, mode='a', newline="") as csv_file:
             csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             csv_writer.writerow(info)
 
-        print(list(info.values()))  # Print data points
+        text = output_text.cget("text") + f"\n{timevalue}, {voltage}, {freq}, {float(channel1)}, {float(channel2)}"
+        output_text.configure(text = text)
         time.sleep(2)
         timevalue += 2
         plot('Time', 'Channel1(X)')
@@ -60,7 +59,37 @@ def plot(xvar, yvar):
     plt.xlabel(xvar)
     plt.ylabel(yvar)
     plt.show()
+    
+def name_btn_clicked():
+    current_time = datetime.datetime.now().strftime("%m-%d-%Y %I.%M%p")
+    csv_file_path = f"C:\\Users\\laqm\\Documents\\CSV Data Outputs\\{name_entry.get()} {current_time}.csv"
+    filepath_text.configure(text = csv_file_path)
+    
+# Driver code for GUI
+root = Tk()
 
-# Driver Code
-my_instrument = pyvisa.ResourceManager().open_resource('GPIB0::8::INSTR')
-data_collect()
+root.title("LAQM Lock-In Amplifier Data Visualizer")
+root.geometry('960x540')
+
+name_text = Label(root, text = "Enter your name (this will go in the CSV file name)")
+name_text.grid(column = 0, row = 0)
+
+name_entry = Entry(root, width = 10)
+name_entry.grid(column = 1, row = 0)
+
+name_btn = Button(root, text = "Enter", command = name_btn_clicked)
+name_btn.grid(column = 2, row = 0)
+
+start_text = Label(root, text = "Begin Program?")
+start_text.grid(column = 0, row = 1)
+
+start_btn = Button(root, text = "Start", command = run)
+start_btn.grid(column = 1, row = 1)
+
+filepath_text = Label(root, text = csv_file_path)
+filepath_text.grid(column = 3, row = 1)
+
+output_text = Label(root, text = "Output values:")
+output_text.grid(column = 0, row = 2)
+
+root.mainloop()
