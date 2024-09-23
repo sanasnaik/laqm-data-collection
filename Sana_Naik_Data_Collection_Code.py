17e41e902e3387aa8d5baff1e9d0488b6414095f
Sana Naik <ssnaik2104@gmail.com>
2024-09-23 15:47:56 -0400

    Fixed live plotting

--- Sana_Naik_Data_Collection_Code.py
+++ Sana_Naik_Data_Collection_Code.py
@@ -14,46 +14,53 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
+# Prepare CSV file path
 current_time = datetime.datetime.now().strftime("%m-%d-%Y %I.%M%p")
-csv_file_path = f"C:\\Users\laqm\Documents\CSV Data Outputs\{current_time}.csv"
-fieldnames = ['Time', 'Voltage', 'Frequency', 'Channel1(X)', 'Channel2(Y)']  # Field names for csv output
-    
-# Collects data into CSV and displays datapoints as a plot
-def data_collect(writer):
-    
-    timevalue = 0
-    print(fieldnames)
+csv_file_path = f"C:\\Users\\laqm\\Documents\\CSV Data Outputs\\{current_time}.csv"
+fieldnames = ['Time', 'Voltage', 'Frequency', 'Channel1(X)', 'Channel2(Y)']
 
-    while True:  # Until program is stopped
-        freq     = my_instrument.query_ascii_values('FREQ?')[0]
-        voltage  = my_instrument.query_ascii_values('SLVL?')[0]
+# Collects data into CSV and displays datapoints as a plot
+def data_collect():
+    timevalue = 0
+
+    # Write header to CSV file
+    with open(csv_file_path, mode='w', newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+    while True:
+        # Query instrument data
+        freq = my_instrument.query_ascii_values('FREQ?')[0]
+        voltage = my_instrument.query_ascii_values('SLVL?')[0]
         channel1 = my_instrument.query_ascii_values("OUTP? 1")[0]
         channel2 = my_instrument.query_ascii_values("OUTP? 2")[0]
 
-        values = [timevalue, voltage, freq, float(channel1), float(channel2)]
-        print(values)
+        # Prepare data for CSV
+        info = {
+            "Time": timevalue,
+            "Voltage": voltage,
+            "Frequency": freq,
+            "Channel1(X)": float(channel1),
+            "Channel2(Y)": float(channel2)
+        }
 
-        writer.writerow(values)
+        # Append data to CSV file
+        with open(csv_file_path, mode='a', newline="") as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            csv_writer.writerow(info)
 
+        print(list(info.values()))  # Print data points
         time.sleep(2)
         timevalue += 2
-        timevalue = round(timevalue, 2)
         plot('Time', 'Channel1(X)')
 
-
-def plot(xvar, yvar):  # NOTE: THIS IS CURRENTLY NOT WORKING
-    
-    data = pd.read_csv(csv_file_path, delimiter=',')
+def plot(xvar, yvar):  
+    data = pd.read_csv(csv_file_path)
     plt.plot(data[xvar], data[yvar])
     plt.xlabel(xvar)
     plt.ylabel(yvar)
     plt.show()
 
-# Initiates CSV file + Driver Code
-with open(csv_file_path, mode='w', newline="") as csv_file:
-    
-    writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(fieldnames)
-
-    my_instrument = pyvisa.ResourceManager().open_resource('GPIB0::8::INSTR')
-    data_collect(writer)
+# Driver Code
+my_instrument = pyvisa.ResourceManager().open_resource('GPIB0::8::INSTR')
+data_collect()
