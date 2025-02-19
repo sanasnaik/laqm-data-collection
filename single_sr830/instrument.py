aca570b605e4919589c1b2fcec02860343577002
Sana Naik <53305879+sanasnaik@users.noreply.github.com>
2025-02-18 22:07:04 -0500

    Fixed thread blocking for freezing

--- single_sr830/instrument.py
+++ single_sr830/instrument.py
@@ -45,22 +45,26 @@ class Instrument:
         with self.autosens_lock:
             self.autosens_thread_running = True
         
-        if self.pymeasure_instrument.is_out_of_range():
-            self.pymeasure_instrument.quick_range()
+        try:
+            if self.pymeasure_instrument.is_out_of_range():
+                self.pymeasure_instrument.quick_range()
 
-        # change sensitivity so that it increases senstivity when it is "0"
-        elif int(self.pymeasure_instrument.ask("SENS?")) > 0:
-            while self.get_voltage() == 0 or self.get_channel1() == 0 or self.get_channel2() == 0:
-                self.pymeasure_instrument.write('LIAE 2,1')
-                self.pymeasure_instrument.write("SENS%d" % (int(self.pymeasure_instrument.ask("SENS?")) - 1))
-                # time.sleep(5.0 * self.pymeasure_instrument.time_constant)
-                self.pymeasure_instrument.write("*CLS")
-                # Set the range as low as possible
-                newsensitivity = 1.15 * abs(self.pymeasure_instrument.magnitude)
-                if self.pymeasure_instrument.input_config in ('I (1 MOhm)', 'I (100 MOhm)'):
-                    newsensitivity = newsensitivity * 1e6
-                self.sens = newsensitivity
-                
-        with self.autosens_lock:
-            self.autosens_thread_running = False
+            # change sensitivity so that it increases senstivity when it is "0"
+            elif int(self.pymeasure_instrument.ask("SENS?")) > 0:
+                retries = 0  # prevent infinite loop
+
+                while self.get_voltage() == 0 or self.get_channel1() == 0 or self.get_channel2() == 0 and retries < 10:
+                    self.pymeasure_instrument.write('LIAE 2,1')
+                    self.pymeasure_instrument.write("SENS%d" % (int(self.pymeasure_instrument.ask("SENS?")) - 1))
+                    # time.sleep(5.0 * self.pymeasure_instrument.time_constant)
+                    self.pymeasure_instrument.write("*CLS")
+                    # Set the range as low as possible
+                    newsensitivity = 1.15 * abs(self.pymeasure_instrument.magnitude)
+                    if self.pymeasure_instrument.input_config in ('I (1 MOhm)', 'I (100 MOhm)'):
+                        newsensitivity = newsensitivity * 1e6
+                    self.sens = newsensitivity
+                    retries += 1
         
+        finally:
+            with self.autosens_lock:
+                self.autosens_thread_running = False
